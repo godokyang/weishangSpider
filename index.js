a2dcbbd5570c7cd9a7052b397141a0da28eb8de1
杨科 <godokyang@qq.com>
2019-11-07 23:47:16 +0800

    新增数据库读取

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const http = require('http')
 const cheerio = require('cheerio')
 const mime = require('mime')
 const url = require('url')
+const mysql = require('mysql');
+const iv = require('iconv-lite')
 const querystring = require('querystring');
 const util = require('util');
 const iconv = require('iconv-lite')
@@ -17,13 +19,29 @@ const requestList = new Map([
   ['loginQrDNS', 'https://open.weixin.qq.com'],
   ['checkLogin', 'https://lp.open.weixin.qq.com/connect/l/qrconnect?'],
   ['tokenDns', 'https://www.szwego.com/service/mp/pc_login_auth.jsp?'],
-  ['shop', 'https://www.szwego.com/service/album/get_album_themes_list.jsp?act=owner&search_value=&search_img=&start_date=&end_date=']
+  ['shop', 'https://www.szwego.com/service/album/get_album_themes_list.jsp?act=owner&search_value=&search_img=&start_date=&end_date='],
+  ['shopOwner', 'https://www.szwego.com/service/album/get_album_list.jsp?act=attention&search_value=&tag_id=&_=']
 ])
+
+//实现本地链接
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '!qaz2wsx',
+  database: 'ChenAnDB'
+})
 
 let dns = 'http://localhost'
 let port = 7000
-let log_goods_id = 'I201908130000538030031868'
+let log_goods_id = null
+try {
+  log_goods_id = fs.readFileSync('log_goods_id')
+} catch (error) {
+  console.log('====================================');
+  console.log(error);
+}
 let log_path = 'error.log'
+let day = -7 // 最多抓取几天的数据
 
 //=>公共方法
 let responseResult = function responseResult(res, returnVal) {
@@ -124,9 +142,12 @@ function setToken(setTokenUrl) {
           cookieWidthToken = item
         }
       })
-
+      
+      let shopOwnerUrl = `${requestList.get('shopOwner')}`
+      connection.query('truncate table ChenAnDB_owner_shop;')
+      getShopOwner(shopOwnerUrl, 1, cookieWidthToken)
       let shopUrl = `${requestList.get('shop')}&shop_id=${shopId}&_=${new Date().getTime()}`
-      return getAllMissingData(shopUrl, 1, cookieWidthToken)
+      getAllMissingData(shopUrl, 1, cookieWidthToken)
     })
     .catch(lerr => {
       logError(log_path, 'getData at last step error', lerr)
@@ -147,6 +168,32 @@ function buildHtml(req, uuid, state) {
   return $.root().html()
 };
 
+function getShopOwner(url, pageIndex, cookieWidthToken) {
+  let target = {
+    url: `${url}&page_index=${pageIndex}`,
+    headers: {
+      cookie: cookieWidthToken
+    }
+  }
+
+  promiseRequest(target, 1000)
+    .then(r => {
+      if (getShopRunningFLag(r)) {
+        getShopOwner(url, ++pageIndex, cookieWidthToken)
+      } else {
+        console.log('owner has real saved!!!')
+      }
+    })
+}
+
+function getShopRunningFLag(r) {
+  let list = JSON.parse(r.body).result.shop_list
+  if (list.length > 0) {
+    writeOwnerToDB(list)
+  }
+  return list && list.length > 0
+}
+
 function getAllMissingData(shopUrl, pageIndex, cookieWidthToken) {
   let target = {
     url: `${shopUrl}&page_index=${pageIndex}`,
@@ -154,18 +201,11 @@ function getAllMissingData(shopUrl, pageIndex, cookieWidthToken) {
       cookie: cookieWidthToken
     }
   }
-  function getRunningFlag(r) {
-    let body = JSON.parse(r.body)
-    let spiderFlag = body.result.goods_list.some(item => {
-      return item.goods_id === log_goods_id
-    })
-    fs.writeFile(`output_${pageIndex}.json`, JSON.stringify(body))
-    return !spiderFlag && body.result.goods_list.length > 0
-  }
-  return promiseRequest(target, 1000)
+  
+  promiseRequest(target, 1000)
     .then(r => {
       if (getRunningFlag(r)) {
-        return getAllMissingData(shopUrl, ++pageIndex, cookieWidthToken)
+        getAllMissingData(shopUrl, ++pageIndex, cookieWidthToken)
       } else {
         console.log('data has real saved!!!')
       }
@@ -173,6 +213,24 @@ function getAllMissingData(shopUrl, pageIndex, cookieWidthToken) {
     .catch(err => {
       logError(log_path, 'get goods error', err)
     })
+}
+
+function getRunningFlag(r) {
+  let list = JSON.parse(r.body).result.goods_list
+  let rejectTime = new Date().getTime() + 1000*60*60*24*day
+  let spiderFlag = list.some(item => {
+    return item.goods_id === log_goods_id
+  })
+  if (!spiderFlag) {
+    spiderFlag = list[list.length - 1].time_stamp < rejectTime
+  }
+  console.log(list[list.length - 1].time_stamp, rejectTime, list[list.length - 1].time_stamp < rejectTime)
+  fs.writeFileSync('log_goods_id', list[list.length - 1].goods_id)
+  if (list.length > 0) {
+    writeProToDB(list)
+  }
+  
+  return !spiderFlag && list.length > 0
 }
 
 function promiseRequest(reqParams, timeout = 100) {
@@ -193,11 +251,43 @@ function promiseRequest(reqParams, timeout = 100) {
 }
 
 function logError(path, title, log) {
-  fs.appendFile(path, `${title}: Date: ${new Date()}`)
-  fs.appendFile(path, '\r\n')
-  fs.appendFile(path, log)
-  fs.appendFile(path, '\r\n')
-  fs.appendFile(path, '\r\n')
+  fs.exists(path, function (exists) {
+    if (exists) {
+      fs.appendFileSync(path, `${title}: Date: ${new Date()}`)
+      fs.appendFileSync(path, '\r\n')
+      fs.appendFileSync(path, log)
+      fs.appendFileSync(path, '\r\n')
+      fs.appendFileSync(path, '\r\n')
+    } else {
+      fs.writeFileSync(path, `${title}: Date: ${new Date()}\r\n\r\n${log}\r\n\r\n`)
+    }
+  })
+
+}
+
+function writeProToDB(list) {
+  let sql = list.reduce((result, item, index,arr) => {
+    return `${result}("${iv.encode(iGetInnerText(item.title), 'utf8')}","${item.videoURL || item.videoUrl}","${item.imgsSrc.toString()}","${item.goods_id}",${item.time_stamp},"${item.shop_id}")${index===(arr.length - 1) ? ';' : ','}`
+  }, 'INSERT INTO ChenAnDB_goods (goods_name, video,images,owner_goods_id,owner_server_time,owner_shop_id) VALUES')
+  connection.query(sql, function (error, results, fields) {
+    logError(log_path, 'Save Data result:', `${new Date()}${error}\r\n${results}`)
+  })
+}
+
+function writeOwnerToDB(list, source = "微商相册", shop_status = 0) {
+  let sql = list.reduce((result, item, index,arr) => {
+    return `${result}("${item.shop_id}","${item.shop_name}","${source}","${item.user_icon}","${"安安的账号"}","${item.shop_url}","${item.total_goods}","${item.new_goods}","${item.followStatus}")${index===(arr.length - 1) ? ';' : ','}`
+  }, 'INSERT INTO ChenAnDB_owner_shop (shop_id, shop_name,source,shop_icon,under_account,shop_url,goods_count,new_goods_count,shop_status) VALUES')
+  connection.query(sql, function (error, results, fields) {
+    logError(log_path, 'Save Data result:', `${new Date()}${error}\r\n${results}`)
+  })
+}
+
+function iGetInnerText(testStr) {
+  let regStr = /[\uD83C|\uD83D|\uD83E][\uDC00-\uDFFF][\u200D|\uFE0F]|[\uD83C|\uD83D|\uD83E][\uDC00-\uDFFF]|[0-9|*|#]\uFE0F\u20E3|[0-9|#]\u20E3|[\u203C-\u3299]\uFE0F\u200D|[\u203C-\u3299]\uFE0F|[\u2122-\u2B55]|\u303D|[\A9|\AE]\u3030|\uA9|\uAE|\u3030/ig;
+  let resultStr = testStr.replace(/[\r\n]/g, ""); //去掉空格
+  resultStr = resultStr.replace(regStr, "")
+  return resultStr;
 }
 
 http.createServer(baseHandle).listen(port)
